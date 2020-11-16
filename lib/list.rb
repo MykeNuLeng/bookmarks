5e1670337f494f3dffcb5443218a17576f1bea92
Michael Linford <linford.mike@gmail.com>
2020-11-16 16:26:04 +0000

    get in loser we're going shopping

--- lib/list.rb
+++ lib/list.rb
@@ -1,8 +1,15 @@
+require 'pg'
 class List
   attr_reader :bookmarks
 
   def self.create
-    @list = Game.new
+    @list = List.new
+  end
+
+  def self.all
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 
   def self.instance
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,5 +1,6 @@
 require 'list'
 
+
 describe List do
    it 'returns a list' do
      expect(subject.bookmarks).to be_an_instance_of Array
@@ -8,4 +9,18 @@ describe List do
    it 'lists the list from lists lists' do
      expect(subject.list_of_bookmarks).to be_an_instance_of String
    end
+
+   it 'lists bookmarks' do
+     expect(subject.bookmarks).to include "Not porn"
+   end
+end
+
+describe '.all' do
+  it 'returns a list of bookmarks' do
+    bookmarks = List.all
+
+    expect(bookmarks).to include "http://www.makersacademy.com"
+    expect(bookmarks).to include "http://www.destroyallsoftware.com"
+    expect(bookmarks).to include "http://www.google.com"
+  end
 end
